7e58eb611d5be2aac4449031536fa0c6c715ee21
Ethem <ethemdenizersoy@gmail.com>
2026-08-08 19:14:53 +0300

    fix: fixed wrong logic in  labtest

--- labtest.py
+++ labtest.py
@@ -91,7 +91,7 @@ def install_pauli_injection(transfer, targets):
   
 
 # --- targets (single-packet tree) -------------------------------------------
-LOSSES     = [(1,1),(1,2)]                       # force depth-2 (every test)
+LOSSES     = [(2,1),(2,2),(3,1),(3,2)]                       # force depth-2 (every test)
 INJECT_ONE = {(1, 0): "X"}                          # 1 error -> corrected
 INJECT_TWO = {(1, 0): "Z",(2, 0): "Y"}             # 2 errors -> miscorrected
 
